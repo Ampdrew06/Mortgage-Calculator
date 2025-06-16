b5850b16c5d9ab6e7d25aea3e9dfc965362ae8fe
Ampdrew06 <ampdrew@gmail.com>
2025-06-16 17:19:42 +0100

    Update PieChart.jsx

--- src/PieChart.jsx
+++ src/PieChart.jsx
@@ -16,11 +16,21 @@ const PieChart = ({ interest, principal }) => {
     ],
   };
 
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false, // Hide default legend
+      },
+    },
+  };
+
   return (
-    <div style={{ maxWidth: '400px', margin: '2rem auto' }}>
-      <Pie data={data} />
-      <div style={{ textAlign: 'center', marginTop: '1rem' }}>
-        <span style={{ color: '#f44336', fontWeight: 'bold' }}>Red:</span> Interest being paid &nbsp;|&nbsp;
+    <div style={{ maxWidth: '300px', height: '300px', margin: '2rem auto' }} role="img" aria-label="Pie chart showing interest versus principal paid">
+      <Pie data={data} options={options} />
+      <div style={{ textAlign: 'center', marginTop: '1rem', fontSize: '0.95rem' }}>
+        <span style={{ color: '#f44336', fontWeight: 'bold' }}>Red:</span> Interest being paid&nbsp;|&nbsp;
         <span style={{ color: '#4caf50', fontWeight: 'bold' }}>Green:</span> Principal being paid
       </div>
     </div>
